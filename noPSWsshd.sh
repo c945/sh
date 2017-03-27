72b2373ad5e2a7d320151f4af65908d27661bc3a
c945 <runner@protom.org>
2017-03-27 18:00:01 +0900

    Idea post

--- noPSWsshd.sh
+++ noPSWsshd.sh
@@ -2,8 +2,15 @@
 #
 # disable password authentication for sshd
 #
-
 /bin/sed -i.prev -e 's/^\(passwordauthentication\)/#\1/i' -e '$aPasswordAuthentication no' /etc/ssh/sshd_config
+
+# Future: enable password authentication from internal address
+# Match address 10.0.0.0/8
+#  PasswordAuthentication yes
+#  PermitRootLogin yes
+
+# Future: check and rollback
+# /usr/sbin/sshd -t || /bin/cp -p /etc/ssh/sshd_config.prev /etc/ssh/sshd_config
 
 # for CentOS6 and CentOS7
 /sbin/service sshd restart
